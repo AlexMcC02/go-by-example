edae5a2e7cc37be70012bcab41b39b87ef50349e
Alex McCullough <alex.mccullough@kainos.com>
2025-10-21 16:00:27 +0100

    if/else

--- main.go
+++ main.go
@@ -4,30 +4,28 @@ import (
 	"fmt"
 )
 
-func main() {
-	i := 1
-	for i <= 3 { // Simple for loop with a single condition (this would be a while loop in other languages).
-		fmt.Println(i)
-		i = i + 1
+func main() { 
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
 	}
 
-	for j := 0; j < 3; j++ { // The classic initial;condition;after for loop.
-		fmt.Println(j)
+	if 8%4 == 0 { // Control flow is simple in go, only notable aspect is that parentheses are not needed.
+		fmt.Println("8 is divisible by 4")
 	}
 
-	for i := range 3 {
-		fmt.Println("range", i) // Accomplishes the same as the above, as i's default value will be 0.
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("either 8 or 7 are even")
 	}
 
-	for { // Will loop endlessly until broken (again, much like a While true statement).
-		fmt.Println("loop")
-		break
-	}
+	// Note: There is no ternary operator in Go.
 
-	for n := range 6 {
-		if n%2 == 0 {
-			continue // You can use the continue keyboard to move to the next iteration of the loop.
-		}
-		fmt.Println(n)
+	if num := 9; num < 0 { // The num variable is available in this and all subsequent branches.
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
 	}
 }
