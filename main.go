8526c6d0d7321c6a7dbc037f2e56d49bd987b6a4
Alex McCullough <alex.mccullough@kainos.com>
2025-10-22 11:24:41 +0100

    maps

--- main.go
+++ main.go
@@ -2,57 +2,39 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"maps"
 )
 
 func main() { 
-	var s []string // Unlike arrays, slices are typed only by the elements they contain.
-	fmt.Println("uninit", s, s == nil, len(s) == 0)
-	// An unitialised slice equals to nil and has a length of 0.
+	m := make(map[string]int) // String key and an int value.
 
-	s = make([]string, 3)
-	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+	m["k1"] = 7 // Setting key value pairs with a standard [key] = value syntax.
+	m["k2"] = 13
 
-	s[0] = "a" // Setting and getting is the same as arrays.
-	s[1] = "b"
-	s[2] = "c"
-	fmt.Println("set:", s)
-	fmt.Println("get:", s[2])
+	fmt.Println("map:", m)
 
-	fmt.Println("len:", len(s)) // Returns the length of the slice as we'd expect.
+	v1 := m["k1"]
+	fmt.Println("v1:", v1) // Get a specific value based on a key.
 
-	// Append is a unique feature of slices, which will return a slice containing one or more new values.
-	s = append(s, "d") 
-	s = append(s, "e", "f") // Note, we need to accept a return value as we may get a new slice value.
-	fmt.Println("apd:", s)
+	v3 := m["k3"]
+	fmt.Println("v3:", v3) // A zero value is returned if the key does not exist.
 
-	c := make([]string, len(s))
-	copy(c, s) // Slices can be copied using the copy function.
-	fmt.Println("cpy:", c)
+	fmt.Println("len:", len(m)) // Returns the number of key value pairs.
 
-	l := s[2:5] // Slice operator used to fetch elements between indices 2 and 5.
-	fmt.Println("sl1:", l)
+	delete(m, "k2") // Delete keyword does, you guessed it, delete things! O.o
+	fmt.Println("map:", m)
 
-	l = s[:5] // Slices up until but excluding s[5].
-	fmt.Println("sl2:", l)
+	clear(m)
+	fmt.Println("map:", m) // Removes all key-value pairs from a map.
 
-	t := []string{"g", "h", "i"} // One line declaration and initialisation.
-	fmt.Println("dcl:", t)
+	_, prs := m["k2"] // Optional return value indicates if a key is present in the map. _ used to discard the value itself.
+	fmt.Println("prs:", prs)
 
-	t2 := []string{"g", "h", "i"} // Comparison of slices.
-	if slices.Equal(t, t2) {
-		fmt.Println("t == t2") // Slices are considered equal to their contents seemingly...?
+	n := map[string]int{"foo": 1, "bar": 2} // Compact init and declare syntax for maps.
+	fmt.Println("map:", n)
+
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) { // Like with slices, we can compare maps.
+		fmt.Println("n == n2")
 	}
-
-	// Slices can be composed into multi-dimensional data structures. The length o inner slices can vary
-	// unlike with multi-dimensional arrays.
-	twoD := make([][]int, 3)
-	for i := range 3 {
-		innerLen := i + 1
-		twoD[i] = make ([]int, innerLen)
-		for j := range innerLen {
-			twoD[i][j] = i + j
-		}
-	}
-	fmt.Println("2d:", twoD)
 }
