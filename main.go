c9e6073eaadb5328d901a5b72769892b6167c826
Alex McCullough <alex.mccullough@kainos.com>
2025-10-22 13:52:03 +0100

    recursion

--- main.go
+++ main.go
@@ -2,32 +2,27 @@ package main
 
 import "fmt"
 
-// Go supports anonymous functions, which can form closures. These are useful when you want to define an
-// inline function without having to name it.
-
-// A closure retains the state of the variables accessed from its surrounding scope (i.e. the function the closure)
-// is nested inside.
-
-// This function returns another function, which we define anonymously in the body of intSeq.
-// The returned function closes over the variable i to form a closure.
-func intSeq() func() int { 
-	i := 0
-	return func() int {
-		i++
-		return i
+func fact(n int) int { // This function will call itself until it reaches the base case of fact(0).
+	if n == 0 {
+		return 1
 	}
+	return n * fact(n-1)
 }
 
 func main() {
-	nextInt := intSeq()
+	fmt.Println(fact(5))
 
-	// Each time intSeq is called, we assign it to nextInt. Because the function value captures 
-	// its own i value, it wil be updated each time we call nextInt.
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
+	// Anonymous functions can also be recursive, but you must explicitly declare a variable with var
+	// to store the function before it's defined.
+	var fib func(n int) int 
 
-	// If we create a new variable and assign intSeq to it, we get a unique, unrelated value.
-	newInts := intSeq()
-	fmt.Println(newInts())
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
