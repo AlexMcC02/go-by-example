55161ccd60ea7d8dea3e4384fc04bcead5603b7e
Alex McCullough <alex.mccullough@kainos.com>
2025-10-22 09:47:27 +0100

    switch

--- main.go
+++ main.go
@@ -2,30 +2,50 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() { 
-	if 7%2 == 0 {
-		fmt.Println("7 is even")
-	} else {
-		fmt.Println("7 is odd")
+	i := 2
+	fmt.Print("Write ", i, " as " )
+	switch i { // There is no need to add a break to the end of a switch case.
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+	
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday: // Commas can be used to separate multiple expressions in the same case.
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
 	}
 
-	if 8%4 == 0 { // Control flow is simple in go, only notable aspect is that parentheses are not needed.
-		fmt.Println("8 is divisible by 4")
+	t := time.Now()
+	switch {
+	case t.Hour() < 12: // Conditional case.
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
 	}
 
-	if 8%2 == 0 || 7%2 == 0 {
-		fmt.Println("either 8 or 7 are even")
+	// A type switch compares types instead of values. You can use this 
+	// to discover the type of an interface value.
+	whatAmI := func(i interface{}) { 
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
 	}
 
-	// Note: There is no ternary operator in Go.
-
-	if num := 9; num < 0 { // The num variable is available in this and all subsequent branches.
-		fmt.Println(num, "is negative")
-	} else if num < 10 {
-		fmt.Println(num, "has 1 digit")
-	} else {
-		fmt.Println(num, "has multiple digits")
-	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
