5edb5bafa0bd6b1c876e5bf2d149eb3a7eb5a66f
Alex McCullough <alex.mccullough@kainos.com>
2025-10-22 11:04:42 +0100

    slices

--- main.go
+++ main.go
@@ -2,38 +2,57 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() { 
-	var a [5]int
-	fmt.Println("emp:", a) // Zero valued by default [0 0 0 0 0].
+	var s []string // Unlike arrays, slices are typed only by the elements they contain.
+	fmt.Println("uninit", s, s == nil, len(s) == 0)
+	// An unitialised slice equals to nil and has a length of 0.
 
-	a[4] = 100 // Set the fifth value to be 100.
-	fmt.Println("set:", a)
-	fmt.Println("get:", a[4]) // Retrieve a specific element by its index.
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
 
-	fmt.Println("len:", len(a)) // Return the length of the array.
+	s[0] = "a" // Setting and getting is the same as arrays.
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
 
-	b := [5]int{1, 2, 3, 4, 5} // One line array initialisation and declaration.
-	fmt.Println("dcl:", b)
+	fmt.Println("len:", len(s)) // Returns the length of the slice as we'd expect.
 
-	b = [...]int{1, 2, 3, 4, 5} // Using [...] will have the compiler apply an appropriate length for you.
-	fmt.Println("dcl:", b)
+	// Append is a unique feature of slices, which will return a slice containing one or more new values.
+	s = append(s, "d") 
+	s = append(s, "e", "f") // Note, we need to accept a return value as we may get a new slice value.
+	fmt.Println("apd:", s)
 
-	b = [...]int{100, 3: 400, 500} // You can use the : symbol to specify a starting index.
-	fmt.Println("idx:", b)
+	c := make([]string, len(s))
+	copy(c, s) // Slices can be copied using the copy function.
+	fmt.Println("cpy:", c)
 
-	var twoD [2][3]int // You can compose types to build multi-dimensional data structures.
-	for i := range 2 {
-		for j := range 3 {
+	l := s[2:5] // Slice operator used to fetch elements between indices 2 and 5.
+	fmt.Println("sl1:", l)
+
+	l = s[:5] // Slices up until but excluding s[5].
+	fmt.Println("sl2:", l)
+
+	t := []string{"g", "h", "i"} // One line declaration and initialisation.
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"} // Comparison of slices.
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2") // Slices are considered equal to their contents seemingly...?
+	}
+
+	// Slices can be composed into multi-dimensional data structures. The length o inner slices can vary
+	// unlike with multi-dimensional arrays.
+	twoD := make([][]int, 3)
+	for i := range 3 {
+		innerLen := i + 1
+		twoD[i] = make ([]int, innerLen)
+		for j := range innerLen {
 			twoD[i][j] = i + j
 		}
-	}
-	fmt.Println("2d:", twoD)
-
-	twoD = [2][3]int { // Initialising a multi-dimensional array.
-		{1, 2, 3},
-		{4, 5 ,6},
 	}
 	fmt.Println("2d:", twoD)
 }
