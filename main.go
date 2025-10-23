7b085f91f197f637bce2ed2c49bf7c7d4bef74e0
Alex McCullough <alex.mccullough@kainos.com>
2025-10-23 11:07:45 +0100

    enums

--- main.go
+++ main.go
@@ -1,66 +1,55 @@
 package main
 
-import (
-	"fmt"
-	"math"
+import "fmt"
+
+// An enumerated type has a fixed number of possible values, each with distinct names.
+// Go does not have a distinct enum type, but they can be implemented using existing language idioms.
+
+// The ServerState type is an int under the hood.
+type ServerState int
+
+// The possible values are defined as constants. The keyword iota generates successive constant values
+// automatically -- 0, 1, 2, 3.
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
 )
 
-// Interfaces are named collections of method signatures.
-type geometry interface {
-	area() float64
-	perim() float64
+// Here we implement the fmt.Stringer interface, allowing us to output values of ServerState as strings.
+var stateName = map[ServerState]string {
+	StateIdle: "idle",
+	StateConnected: "connected",
+	StateError: "error",
+	StateRetrying: "retrying",
 }
 
-type rect struct {
-	width, height float64
+func (ss ServerState) String() string {
+	return stateName[ss]
 }
 
-type circle struct {
-	radius float64
-}
-
-// To implement an interface in Go, you implement all the methods in the interface.
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
-}
-
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-// If a variable has an interface type, we can call methods that are in the named interface.
-// This function is a generic and will work on any geometry.
-func measure(g geometry) {
-	fmt.Println(g)
-	fmt.Println(g.area())
-	fmt.Println(g.perim())
-}
-
-// It can be useful to know the runtime type of an interface value.
-// One option is using a type assertion, as shown here, or a type switch.
-func detectCircle(g geometry) {
-	if c, ok := g.(circle); ok {
-		fmt.Println("circle with radius", c.radius)
-	}
-}
-
+// Though ServerState is implemented as an int, passing an int to transition will result in a type
+// mismatch, providing some degree of compile-time type safety for enums.
 func main() {
-	r := rect{width: 3, height: 4}
-	c := circle{radius: 5}
+	ns := transition(StateIdle)
+	fmt.Println(ns)
 
-	// The circle and rect struct types both implement the geomtry interface so we can use instances
-	// of these structs as arguments to measure.
-	measure(r)
-	measure(c)
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+}
 
-	detectCircle(r)
-	detectCircle(c)
+// This function emulates a state transition for a web server, it takes the existing state 
+// and returns a new state.
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unkwown state: %s", s))
+	}
 }
