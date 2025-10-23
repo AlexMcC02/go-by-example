cbb17717eaf30406443f65b0ab255354da836a09
Alex McCullough <alex.mccullough@kainos.com>
2025-10-23 09:37:16 +0100

    strings and runes

--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
-import "fmt"
-
-// This function passes by value, meaning it will get a copy of ival distinct from the one 
-// in the calling function. As it does not return the value, the value passed in from the
-// scope where the function is called will remain the same.
-func zeroval(ival int) { 
-	ival = 0
-}
-
-// Unlike the above function, this one specifies a *int parameter. This is an int pointer.
-// The *iptr code will dereference the pointer from its memory address to the current value 
-// at the address. Assigning a value to the dereferenced point changes the value at the
-// referenced address.
-
-// Simply put, this function, despite not returning anything, will be able to modify the
-// value the pointer references.
-func zeroptr(iptr *int) {
-	*iptr = 0
-}
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	i := 1
-	fmt.Println("initial:", i)
+	// A Go string is read-only sequence of bytes.
+	// The language and standard library treat strings as containers of text encoded in UTF-8.
+	// In other languages, strings are made of "characters".
+	// In Go, the concept of a character is called a Rune, an integer that represents a Unicode code point.
 
-	// Zeroval does not change the i defined here, but zeroptr does as it has a reference to the
-	// memory address for that variable.
-	zeroval(i)
-	fmt.Println("zeroval", i)
+	// S is a string assigned a literal value representing the word "hello" in the Thai language.
+	// Go string literals are UTF-8 encoded text.
+	const s = "สวัสดี"
 
-	zeroptr(&i) // The ampersand means you are providing the memory adress of i (or a pointer to i).
-	fmt.Println("zeroptr", i)
+	// Because strings are equivalent to []byte, this statement will produce the length of the 
+	// raw bytes stored within.
+	fmt.Println("Len:", len(s))
 
-	fmt.Println("pointer", &i)
+	// Indexing a string produces the raw byte values at each index. This loop will output the
+	// hexidecimal values of all the bytes that constitute the code points in s.
+	for i := range len(s) {
+		fmt.Printf("%x", s[i])
+	}
+	fmt.Println()
+
+	// To count the number of runes in as tring, we use the utf8 package. Because some Thai characters
+	// are represented by UTF-8 code points that can span multiple bytes, the result of this count may
+	// be larger than you expected.
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	// A range loop handles strings in a special way, decoding each rune with its offset in the string.
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+
+	// The same results can be achieved using the utf8.DecodeRuneInString function.
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeLastRuneInString(s[i:])
+		fmt.Printf("%#U starts at %d\n", runeValue, i)
+		w = width
+
+		// Passing a rune to a function.
+		examineRune(runeValue)
+	}
+}
+
+// Values enclosed in single quotes are rune literals. We can compare a rune value to a rune literal directly.
+func examineRune(r rune) {
+	if r == 't' {
+		fmt.Println("found tee")
+	} else if r == 'ส' {
+		fmt.Println("found so sua")
+	}
 }
