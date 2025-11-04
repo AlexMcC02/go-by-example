b02523fe19fd05c1457a54fc70b74ef86483facd
Alex McCullough <alex.mccullough@kainos.com>
2025-11-04 13:29:30 +0000

    range-over-iterators

--- main.go
+++ main.go
@@ -1,60 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"iter"
+	"slices"
+)
 
-// SlicesIndex is an example of a generic function, with it taking a slice of any comparable
-// type and an element of that type and returns the index of the first ocurrence fo v in s, or
-// -1 if not present. Note: being comparable means you can apply the == and != operators.
-func SlicesIndex[S ~ []E, E comparable](s S, v E) int {
-	for i := range s {
-		if v == s[i] {
-			return i
-		}
-	}
-	return -1
-}
-
-// List is an example of a generic type, it being a singly linked-list with values of any type.
-type List[T any] struct {
+type List[T any] struct { // A singly linked-list, this time taking advantage of Go's iterators.
 	head, tail *element[T]
 }
+
 type element[T any] struct {
 	next *element[T]
 	val T
 }
 
-// We can define methods on generic types but we must keep the type parameters in place.
-// E.g. the type is List[T] not List.
 func (lst *List[T]) Push(v T) {
 	if lst.tail == nil {
 		lst.head = &element[T]{val: v}
 		lst.tail = lst.head
 	} else {
-		lst.tail.next = &element[T]{val: v}
+		lst.tail.next = &element[T]{val :v}
 		lst.tail = lst.tail.next
 	}
 }
 
-// AllElements returns all the List elements as a slice.
-func (lst *List[T]) AllElements() []T {
-	var elems []T
-	for e := lst.head; e != nil; e = e.next {
-		elems = append(elems, e.val)
+// Reminder: an iterator provides a way to access and traverse and access the elements of a collection, 
+// one by one, without exposing the underlying data structure.
+
+// All returns an iterator, which in Go is a function with a special signature.
+// The iterator function takes another function as a parameter. By convention, this function is typically named yield.
+// The iterator will then call yield for every element we want to iterate over. 
+// You can take advantage of yield's return value for an early termination.
+func (lst *List[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+		}
 	}
-	return elems
 }
 
-// When invoking generic functions, we can often rely on type inference.
+// This function generates fibonacci numbers, and it will keep running as long as yield returns true.
+func genFib() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		a, b := 1, 1
+
+		for {
+			if !yield(a) {
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
+
 func main() {
-	var s = []string{"foo", "bar", "zoo"}
-
-	fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
-
-	_ = SlicesIndex(s, "zoo")
-	
 	lst := List[int]{}
 	lst.Push(10)
 	lst.Push(13)
 	lst.Push(23)
-	fmt.Println("list:", lst.AllElements())
+	
+	//List.All returns an interator, meaning it can be used in a regular range loop.
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	// The Slices package contains the Collect function, which will take any iterator and collects all its values into a slice.
+	all := slices.Collect(lst.All())
+	fmt.Println("all:", all)
+
+	// As soon as the loop hits break or early return, the yield function passed to the iterator will return false.
+	for n := range genFib() {
+		if n >= 10 {
+			break
+		}
+		fmt.Println(n)
+	}
 }
