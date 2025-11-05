d2119febaea38ae8b4a40b3aeaf37c5c315af699
Alex McCullough <alex.mccullough@kainos.com>
2025-11-05 10:39:45 +0000

    goroutines

--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
-// In Go, it is possible to define custom error types by implementing the Error() method on them.
+// A goroutine is a lightweight thread of execution.
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
-type argError struct { // A custom error type will usually have the suffix "Error".
-	arg int
-	message string
-}
-
-func (e *argError) Error() string { // Adding this Error method makes argError implement the error interface.
-	return fmt.Sprint("%d - %s", e.arg, e.message)
-}
-
-func f(arg int) (int, error) {
-	if arg == 42 {
-		return -1, &argError{arg, "can't work with it"} // Returning the custom error.
+func f(from string) {
+	for i := range 3 {
+		fmt.Println(from, ":", i)
 	}
-	return arg + 3, nil
 }
 
-// errors.As is a more advanced version of errors.Is. It checks that a given error (or any error in its chain) matches a specific error
-// type and converts to a value of that type, returning true. If there's no match, it will return false.
 func main() {
-	_, err := f(42)
-	var ae *argError
-	if errors.As(err, &ae) {
-		fmt.Println(ae.arg)
-		fmt.Println(ae.message)
-	} else {
-		fmt.Println("err doesn't match argError")
-	}
+	f("direct") // This is the usual way we'd call a function -- it is run synchronously.
+	go f("goroutine") // This is how we invoke a goroutine, it will execute concurrently with the calling one.
+	go func(msg string) { // You can also start a goroutine for an anonymous function call.
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second) // The two function calls are running asynchronously in separate goroutines. Here we wait for them to finish.
+	fmt.Println("done") // The synchronous function will always execute first, as it synchronous execution is 'blocking'.
 }
