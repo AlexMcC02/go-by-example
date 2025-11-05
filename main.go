51f918b8739711db592b3daa46a3c69d8ab184e3
Alex McCullough <alex.mccullough@kainos.com>
2025-11-05 11:45:40 +0000

    channel-synchronisation

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
 
-// By default, channels are unbuffered.
-// This means that channels will only accept sends (chan <-) if there is a corresponding receive (<- chan).
-// Buffered channels on the other hand accept a limited number of values without a corresponding receiver for said values.
+// Channels can be used to synchronise execution across goroutines.
+
+// In this function, the done channel will be used to notify another goroutine that this function's work is done.
+func worker(done chan bool) {
+    fmt.Print("working...")
+    time.Sleep(time.Second)
+    fmt.Println("done")
+
+    done <- true // Sending a value to notify that the function is done.
+}
 
 func main() {
 
-	// A channel of strings buffering up to two values.
-    messages := make(chan string, 2)
+    done := make(chan bool, 1) // Starting a worker goroutine, giving it a channel to notify on.
+    go worker(done) 
 
-	// As the channel is buffered, we don't need to a corresponding concurrent receive.
-    messages <- "buffered"
-    messages <- "channel"
-
-    fmt.Println(<-messages)
-    fmt.Println(<-messages)
+    <-done // Block until we receive a notification from the worker on the channel.
 }
