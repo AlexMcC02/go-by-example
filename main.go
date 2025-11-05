1c9ea4e59e4271a7c44b29180c7359fe5c854ece
Alex McCullough <alex.mccullough@kainos.com>
2025-11-05 11:17:01 +0000

    channels

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
-// A goroutine is a lightweight thread of execution.
+import "fmt"
 
-import (
-	"fmt"
-	"time"
-)
-
-func f(from string) {
-	for i := range 3 {
-		fmt.Println(from, ":", i)
-	}
-}
+// Channels are the pipes that connect concurrent goroutines. You can send values 
+// into channels from one goroutine and receive those values into another goroutine.
 
 func main() {
-	f("direct") // This is the usual way we'd call a function -- it is run synchronously.
-	go f("goroutine") // This is how we invoke a goroutine, it will execute concurrently with the calling one.
-	go func(msg string) { // You can also start a goroutine for an anonymous function call.
-		fmt.Println(msg)
-	}("going")
 
-	time.Sleep(time.Second) // The two function calls are running asynchronously in separate goroutines. Here we wait for them to finish.
-	fmt.Println("done") // The synchronous function will always execute first, as it synchronous execution is 'blocking'.
+	// New channels are created with make, with them being typed by the values they convey.
+    messages := make(chan string)
+
+	// Here a value is sent into a channel using the <- syntax.
+    go func() { messages <- "ping" }()
+
+	// The <-channel syntax receives a value from the channel.
+    msg := <-messages
+
+	// Upon running the program, the 'ping' message will be passed from one goroutine to another via the messages channel.
+	// By default send and receives block until both sender and receiver a ready.
+    fmt.Println(msg)
 }
