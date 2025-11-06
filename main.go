32b0dd7e374c817306daf10e5592cc95886c4c22
Alex McCullough <alex.mccullough@kainos.com>
2025-11-06 11:25:13 +0000

    closing-channels

--- main.go
+++ main.go
@@ -2,42 +2,46 @@ package main
 
 import "fmt"
 
-// Standard send and receive operations on channels are blocking.
-// Select, when used with a default clause, can implement non-blocking
-// sends, receives, multi-way selects.
+// Channels should be closed when there are no more values to be sent to it.
+// This can communicate a completion state to the channel's receivers.
 
 func main() {
-	messages := make(chan string)
-	signals := make(chan bool)
+	
+	// A jobs channel will communicate the work to be completed by a worker goroutine.
+	// When no more jobs need to be completed, the jobs channel will be closed.
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-	// This is an example of a non-blocking receive.
-	// If a value is available on the messages channel, select will take the appropriate case
-	// otherwise, it will take the default case. This means something will always be returned.
-	select {
-	case msg := <-messages:
-		fmt.Println("received message", msg)
-	default:
-		fmt.Println("no message received")
-	}
+	// This is the worker goroutine, it repeatedly receives from jobs.
+	// The more value indicates if there's more jobs to receive, if it
+	// returns false, it means the jobs channel has been closed.
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
 
-	// A non-blocking send is similar. msg cannot be sent to the messages cahannel, as it
-	// has no buffer and there's no receiver. The default case will be selected.
-	msg := "hi"
-	select {
-	case messages <- msg:
-		fmt.Println("sent message", msg)
-	default:
-		fmt.Println("no message sent")
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
 	}
+	close(jobs)
+	fmt.Println("sent all jobs")
 
-	// Multiple cases above the default case can be used to implement multi-way, 
-	// non-blocking select.
-	select {
-	case msg := <-messages:
-		fmt.Println("received message", msg)
-	case sig := <-signals:
-		fmt.Println("received signal", sig)
-	default:
-		fmt.Println("no activity")
-	}
+	<-done
+
+	// Reading from a closed channel succeeds immediately, returning the zero
+	// value of the underlying type. The optional second return value is true if
+	// the value received was delivered by a successful send operation to the
+	// channel, or false if it was a zero value (generated because the channel is
+	// both closed and empty).
+	_, ok := <-jobs
+	fmt.Println("received more jobs:", ok)
 }
