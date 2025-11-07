4025c7e0440b8d7f031cdbfbf691dea58bf73416
Alex McCullough <alex.mccullough@kainos.com>
2025-11-07 14:16:50 +0000

    sorting-by-functions

--- main.go
+++ main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
 
-// Go's slices package implements sorting for builtins and user-defined types.
-// This file contains examples of sorting on builtin types.
+// There are times you'll want to sort a collection by something other than its natural order.
+// E.g. -- we want to sort strings by their length instead of alphabetically.
 
 func main() {
-	// Sorting functions are generic, and work for any ordered built-in type.
-	strs := []string{"c", "a", "b"}
-	slices.Sort(strs)
-	fmt.Println("Strings:", strs)
+	fruits := []string{"peach", "banana", "kiwi"}
 
-	// An example of sorting ints.
-	ints := []int{7, 2, 4}
-	slices.Sort(ints)
-	fmt.Println("Ints:", ints)
+	// We implement a comparison function for string lengths (cmp.Compare is helpful for this).
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
 
-	// We can also use the slices package to check if a slice is already in sorted order.
-	s := slices.IsSorted(ints)
-	fmt.Println("Sorted:", s)
+	// Now we can call slices.SortFunc with this custom comparison to sort fruits by name length.
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
+
+	// The same technique can be employed to sort a slice of custom values.
+	type Person struct {
+		name string
+		age int
+	}
+
+	people := []Person{
+		{name: "Jax", age: 37},
+		{name: "TJ", age: 25},
+		{name: "Alex", age: 72},
+	}
+
+	// This implementation sorts the custom type of Person by their age.
+	// For performance reasons, it may be desirable to pass my reference rather than value with a pointer.
+	slices.SortFunc(people,
+		func(a, b Person) int {
+			return cmp.Compare(a.age, b.age)
+		})
+	fmt.Println(people)
 }
