3a15acab337a4f2a57b0b867f4e7f4f2eca2378c
Alex McCullough <alex.mccullough@kainos.com>
2025-11-07 10:33:15 +0000

    atomic-counters

--- main.go
+++ main.go
@@ -1,58 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "sync"
+    "sync/atomic"
 )
 
-// Rate limiting is a key mechanism for controlling resource utilisation and maintaining quality of service.
-// Go elegantly supports rate limiting with goroutines, channels, and tickers.
+// Communication over channels is the primary means of state management. This was demonstrated with
+// worker pools. Another means to achieve this is through atomic counters.
 
 func main() {
-	// Below is an example of basic rate limiting. For example, if we want to limit how incoming requests are handled.
-	requests := make(chan int, 5)
-	for i := 1; i <= 5; i++ {
-		requests <- i
-	}
-	close(requests)
 
-	// The limited channel receives a value every 200 milliseconds, this will serve to regulate the rate limit.
-	limiter := time.Tick(200 * time.Millisecond)
+	// Declaring an atomic integer type.
+    var ops atomic.Uint64
 
-	// The limiter is implemented by blocking on a receive from it, this will limit the application to processing
-	// 1 request every 200 milliseconds.
-	for req := range requests {
-		<-limiter
-		fmt.Println("request", req, time.Now())
-	}
+	// This wait group will help us wait for all goroutines to finish their work.
+    var wg sync.WaitGroup
 
-	// It may be desirable to have short bursts of requests withion the rate limiting scheme, whilst preserving
-	// the overall limit. This can be accomplished by buffering the limiter channel. This channel will allow bursts
-	// of up to 3 events.
-	burstyLimiter := make(chan time.Time, 3)
+	// Here we start 50 goroutines that each increment the counter 1000 times.
+    for range 50 {
+        wg.Go(func() {
+            for range 1000 {
 
-	// Simulating filling the buffer.
-	for range 3 {
-		burstyLimiter <- time.Now()
-	}
+                ops.Add(1)
+            }
+        })
+    }
 
-	// In this example, the program will attempt to add a new value to the burstyLimiter, up to its limit of 3.
-	go func() {
-		for t := range time.Tick(200 * time.Millisecond) {
-			burstyLimiter <- t
-		}
-	}()
+	// Call the wait group to block until all goroutines are done.
+    wg.Wait()
 
-	// In this example, 5 more incoming requests are simulated. The first 3 will benefit from the capacity of
-	// the burstyLimiter.
-	burstyRequests := make(chan int, 5)
-	for i := 1; i <= 5; i++ {
-		burstyRequests <- i
-	}
-	close(burstyRequests)
-
-	for req := range burstyRequests {
-		<-burstyLimiter
-		fmt.Println("request", req, time.Now())
-	}
+	// Here no goroutines are writing to ops, but using Load it's safe to atomically read
+	// a value even while other goroutines are atomically updating it.
+    fmt.Println("ops:", ops.Load())
 }
