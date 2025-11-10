1471e8135f9a4063a4c0f35611990d6755e090c3
Alex McCullough <alex.mccullough@kainos.com>
2025-11-10 11:41:41 +0000

    regular-expressions

--- main.go
+++ main.go
@@ -1,69 +1,65 @@
 package main
 
 import (
-	"os"
-	"text/template"
+	"bytes"
+	"fmt"
+	"regexp"
 )
 
-// Go offers built-in support for creating dynamic content or showing customised output to the user
-// with the text/tempalte package.
+// Go offers built-in support for regular expressions.
 
 func main() {
-
-	// Here a new template is created and its body is parsed from a string.
-	// Templates consist of a mix of a static text and actions enclosed {{...}}
-	// that can be used to dynamically insert content.
-	t1 := template.New("t1")
-	t1, err := t1.Parse("Value is {{.}}\n")
-	if err != nil {
-		panic(err)
-	}
-
-	// The template.Must function can be used to panic in case Parse returns an error.
-	t1 = template.Must(t1.Parse("Value: {{.}}\n"))
-
-	// By executing the template we generatge its text with specific values for its actions.
-	// Here the {{.}} action is replaced by the value passed as a parameter to Execute.
-	t1.Execute(os.Stdout, "some text")
-	t1.Execute(os.Stdout, 5)
-	t1.Execute(os.Stdout, []string {
-		"Go",
-		"Rust",
-		"C++",
-		"C#",
-	})
-
-	// Helper function that will be used later.
-	Create := func(name, t string) *template.Template {
-		return template.Must(template.New(name).Parse(t))
-	}
 	
-	// If the data is a struct, the {{.FieldName}} action is used to access it fields.
-	t2 := Create("t2", "Name: {{.Name}}\n")
-	t2.Execute(os.Stdout, struct {
-		Name string
-	}{"Jane Doe"})
+	// This tests whether a pattern matches a string.
+	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	fmt.Println(match)
 
-	// A similar approach is used for maps.
-	t2.Execute(os.Stdout, map[string]string {
-		"Name": "Mickey Mouse",
-	})
+	// Above a string pattern was used directly, but for other regexp tasks you'll
+	// need to compile an optimised Regexp struct.
+	r, _ := regexp.Compile("p([a-z]+)ch")
 
-	// If/else provide conditional execution for templates.
-	// A value is considered 'false' if it's the default value for a type.
-	t3 := Create("t3",
-        "{{if . -}} yes {{else -}} no {{end}}\n")
-    t3.Execute(os.Stdout, "not empty")
-    t3.Execute(os.Stdout, "")
+	// Matching for the presence of a string.
+	fmt.Println(r.MatchString("peach"))
+	
+	// Finds the match for the regexp.
+	fmt.Println(r.FindString("peach punch"))
 
-	// Range blocks can be used for looping.
-	t4 := Create("t4",
-        "Range: {{range .}}{{.}} {{end}}\n")
-    t4.Execute(os.Stdout,
-        []string{
-            "Go",
-            "Rust",
-            "C++",
-            "C#",
-        })
+	// Returns the start and indexes for the match instead of the matching text.
+	fmt.Println("idx:", r.FindStringIndex("peach peach"))
+
+	// This function will include information about both the whole-pattern matches
+	// and the submatches within those matches.
+	fmt.Println(r.FindStringSubmatch("peach peach"))
+
+	// This will return the indexes of the matches and submatches.
+	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
+
+	// The all variants of therse functions to apply to all matches
+	// in the input, not just the first.
+	fmt.Println(r.FindAllString("peach punch pinch", -1))
+
+	// Similar case as above.
+	fmt.Println("all:", r.FindAllStringSubmatchIndex(
+        "peach punch pinch", -1))
+
+	// Supplying a non-negative second argument will limit the 
+	// number of matches.
+	fmt.Println(r.FindAllString("peach punch pinch", 2))
+
+	// In addition to strings, []byte arguments can be supplied instead.
+	fmt.Println(r.Match([]byte("peach")))
+
+	// MustCompile makes it safer to make use of global variables for regular
+	// expressions, as it will panic instead of returning an error.
+	r = regexp.MustCompile("p([a-z]+)ch")
+    fmt.Println("regexp:", r)
+
+	// The regexp package also enables the replacing of string subsets.
+	fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
+
+	// Finally, the Func variant allows you to transform the matched
+	// text within a given fujnction.
+	in := []byte("a peach")
+    out := r.ReplaceAllFunc(in, bytes.ToUpper)
+    fmt.Println(string(out))
 }
