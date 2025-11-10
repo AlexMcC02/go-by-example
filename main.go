db032b68cda1a214473ffb37ed12e4218a12bae1
Alex McCullough <alex.mccullough@kainos.com>
2025-11-10 14:15:26 +0000

    xml

--- main.go
+++ main.go
@@ -1,111 +1,62 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "os"
-    "strings"
+	"encoding/xml"
+	"fmt"
 )
 
-// Go offers built-in support for JSON encoding and decoding, including to
-// and from built-in and custom data types.
+// Go offers built-in support for XML and XML-like formats with
+// the encoding/xml package.
 
-// We'll use these two structs to demonstrate encoding and decoding of custom
-// types below.
-type response1 struct {
-    Page   int
-    Fruits []string
+// Plant will be mapped to XML. Here we use the special features of
+// the XML package: the XMLName field dictates the name of the
+// XML element representing this struct; id, attr means that if the
+// field is an XML attribute rather than a nested element.
+type Plant struct {
+	XMLName xml.Name `xml:"plant"`
+	Id int `xml:"id,attr"`
+	Name string `xml:"name"`
+	Origin []string `xml:"origin"`
 }
 
-type response2 struct {
-    Page   int      `json:"page"`
-    Fruits []string `json:"fruits"`
+func (p Plant) String() string {
+	return fmt.Sprintf("Plant id=%v, name=%v, origin=%v", p.Id, p.Name, p.Origin)
 }
 
 func main() {
+	coffee := &Plant{Id: 27, Name: "Coffee"}
+	coffee.Origin = []string{"Ethiopia", "Brazil"}
 
-	// Below are some examples of encoding basic data types to JSON.
-	// Note: fields must start with capital letters to be exported.
-    bolB, _ := json.Marshal(true)
-    fmt.Println(string(bolB))
+	// Emit XML representing the plant, with MarshalIndent used to provide
+	// a human readable output.
+	out, _ := xml.MarshalIndent(coffee, " ", "  ")
+	fmt.Println(string(out))
 
-    intB, _ := json.Marshal(1)
-    fmt.Println(string(intB))
+	// Explicit appending of an XML header.
+	fmt.Println(xml.Header + string(out))
 
-    fltB, _ := json.Marshal(2.34)
-    fmt.Println(string(fltB))
+	// Unmarshal is used to parse a string of bytes with XML into a data
+	// structure. If the XML is malformed or cannot be mapped onto Plant, a
+	// descriptive error will be returned.
+	var p Plant
+	if err := xml.Unmarshal(out, &p); err != nil {
+		panic(err)
+	}
+	fmt.Println(p)
 
-    strB, _ := json.Marshal("gopher")
-    fmt.Println(string(strB))
+	tomato := &Plant{Id: 81, Name: "Tomato"}
+	tomato.Origin = []string{"Mexico", "California"}
 
-	// Here are some examples for slices are maps, which encode to
-	// JSON arrays and objects as you'd expect.
-    slcD := []string{"apple", "peach", "pear"}
-    slcB, _ := json.Marshal(slcD)
-    fmt.Println(string(slcB))
+	type Nesting struct {
+		XMLName xml.Name `xml:"nesting"`
+		Plants []*Plant `xml:"parent>child>plant"`
+	}
 
-    mapD := map[string]int{"apple": 5, "lettuce": 7}
-    mapB, _ := json.Marshal(mapD)
-    fmt.Println(string(mapB))
+	// The parent>child>plant field tag tells the encoder to test
+	// all plants under <parent><child>.
+    nesting := &Nesting{}
+    nesting.Plants = []*Plant{coffee, tomato}
 
-	// The JSON package can automatically encode your custom data types.
-	// It will only include exported fields in the encoded output and will
-	// by default use those names as the JSON keys.
-    res1D := &response1{
-        Page:   1,
-        Fruits: []string{"apple", "peach", "pear"}}
-    res1B, _ := json.Marshal(res1D)
-    fmt.Println(string(res1B))
-
-	// You can use tags on struct field declarations to custgomize the encoded
-	// JSON key names.
-    res2D := &response2{
-        Page:   1,
-        Fruits: []string{"apple", "peach", "pear"}}
-    res2B, _ := json.Marshal(res2D)
-    fmt.Println(string(res2B))
-
-	// Here is an example of a generic data structure.
-    byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
-
-	// Here is the variable that will be used to store the decoded
-	// JSON data. This syntax will hold a map of strings to arbitray data types.
-    var dat map[string]interface{}
-
-	// The decoding proper, with an appropriate error check.
-    if err := json.Unmarshal(byt, &dat); err != nil {
-        panic(err)
-    }
-    fmt.Println(dat)
-
-	// Type conversion.
-    num := dat["num"].(float64)
-    fmt.Println(num)
-
-	// Accessing nested data requires a series of conversions.
-    strs := dat["strs"].([]interface{})
-    str1 := strs[0].(string)
-    fmt.Println(str1)
-
-	// JSON can be decoded into custom data types.
-    str := `{"page": 1, "fruits": ["apple", "peach"]}`
-    res := response2{}
-    json.Unmarshal([]byte(str), &res)
-    fmt.Println(res)
-    fmt.Println(res.Fruits[0])
-
-	// Whereas in previous examples bytes and stgrings were used as intermediates
-	// between the data and JSON representation on standard out. We can also stream
-	// JSON encodings directly to os.Writers like os.Stdout or even HTTP response
-	// bodies.
-    enc := json.NewEncoder(os.Stdout)
-    d := map[string]int{"apple": 5, "lettuce": 7}
-    enc.Encode(d)
-
-	// Streaming reads from os.Readers like os.Stdin or HTTP request bodies is done with
-	// json.Decoder.
-    dec := json.NewDecoder(strings.NewReader(str))
-    res1 := response2{}
-    dec.Decode(&res1)
-    fmt.Println(res1)
+    out, _ = xml.MarshalIndent(nesting, " ", "  ")
+    fmt.Println(string(out))
 }
