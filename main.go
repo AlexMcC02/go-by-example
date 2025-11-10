52697f011748c2a4811ffee3630eb6a27489979c
Alex McCullough <alex.mccullough@kainos.com>
2025-11-10 15:21:19 +0000

    epoch

--- main.go
+++ main.go
@@ -5,50 +5,22 @@ import (
 	"time"
 )
 
-// Go offers extensive support for times and durations.
+// A common requirement in programs is getting the number of seconds, milliseconds,
+// or nanoseconds since the Unix epoch.
 
 func main() {
-	p := fmt.Println
 
-	// Getting the current time.
+	// Use time.Now with Unix, UnixMilli or UnixNano to get elapsed time since the
+	// Unix epoch in seconds, milliseconds, or nanoseconds, respectively.
 	now := time.Now()
-	p(now)
+	fmt.Println(now)
 
-	// You can build a time struct by providing the year, month, day, etc.
-	// Times are ALWAYS associated with a timezone, here UTC is provided.
-	then := time.Date(
-		2009, 11, 17, 20, 34, 58, 651386237, time.UTC)
-	p(then)
+	fmt.Println(now.Unix())
+	fmt.Println(now.UnixMilli())
+	fmt.Println(now.UnixNano())
 
-	// You can extract the various componets of the time value as expected.
-	p(then.Year())
-	p(then.Month())
-	p(then.Day())
-	p(then.Hour())
-	p(then.Minute())
-	p(then.Second())
-	p(then.Nanosecond())
-	p(then.Location())
-
-	// Monday-Sunday is also available.
-	p(then.Weekday())
-
-	// These methods will compare two times.
-	p(then.Before(now))
-	p(then.After(now))
-	p(then.Equal(now))
-
-	// The sub method returns a Duration representing the interval between two times.
-	diff := now.Sub(then)
-	p(diff)
-
-	// We can compute the length of the duration in various units.
-	p(diff.Hours())
-	p(diff.Minutes())
-	p(diff.Seconds())
-	p(diff.Nanoseconds())
-
-	// You can use Add to advance time.
-	p(then.Add(diff))
-	p(then.Add(-diff))
+	// You can also convert integer seconds or nanoseconds since the epoch into the
+	// corresponding time.
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
 }
