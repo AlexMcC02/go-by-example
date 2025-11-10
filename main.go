4005fd244fd57a438b88ca87682259f7f5aa5290
Alex McCullough <alex.mccullough@kainos.com>
2025-11-10 09:10:49 +0000

    string-functions

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	s "strings"
+)
 
-// Recover is a built-in function that allows you to recover from a panic.
-// A recover can stop a panic from aborting the program and let it continue with execution instead.
+// The standard library's strings package provides many useful string-related functions.
+// This file contains several examples.
 
-// This function will panic.
-func mayPanic() {
-	panic("a problem")
-}
+// Applies an alias to the print function.
+var p = fmt.Println
 
 func main() {
-	// Recover must be called within a deferred function.
-	// When the enclosing function panics, the defer will activate and a recover call within it will catch the panic.
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered. Error:\n", r)
-		}
-	}()
-
-	mayPanic()
-
-	// This code won't run as mayPanic() will panic.
-	// The execution of main will cease at this point and resume in the deferred closure.
-	fmt.Println("After myPanic()")
+    p("Contains:  ", s.Contains("test", "es"))
+    p("Count:     ", s.Count("test", "t"))
+    p("HasPrefix: ", s.HasPrefix("test", "te"))
+    p("HasSuffix: ", s.HasSuffix("test", "st"))
+    p("Index:     ", s.Index("test", "e"))
+    p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+    p("Repeat:    ", s.Repeat("a", 5))
+    p("Replace:   ", s.Replace("foo", "o", "0", -1))
+    p("Replace:   ", s.Replace("foo", "o", "0", 1))
+    p("Split:     ", s.Split("a-b-c-d-e", "-"))
+    p("ToLower:   ", s.ToLower("TEST"))
+    p("ToUpper:   ", s.ToUpper("test"))
 }
