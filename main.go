d9182a5903b52a07aa3c4d80887be5913edc5537
Alex McCullough <alex.mccullough@kainos.com>
2025-11-13 09:38:22 +0000

    random-nums

--- main.go
+++ main.go
@@ -2,42 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math/rand/v2"
 )
 
-// Go supports time formatting and parsing via pattern-based layouts.
+// Go's math/rand/v2 package provides pseudorandom number generation.
 
 func main() {
-	p := fmt.Println
 
-	// Here we format the output according to RFC3339.
-	t := time.Now()
-	p(t.Format(time.RFC3339))
+	// For example, rand.IntN returns a random int n, 0 <= n < 100.
+	fmt.Print(rand.IntN(100), ",")
+	fmt.Print(rand.IntN(100))
+	fmt.Println()
 
-	// Time parsing uses the same layout values as Format.
-	t1, e := time.Parse(
-        time.RFC3339,
-        "2012-11-01T22:08:41+00:00")
-    p(t1)
+	// rand.Float64 returns a float64 f, 0.0 <= f < 1.0
+	fmt.Println(rand.Float64())
 
-	// Format and Parse use example-based layout. These layouts must use the
-	// reference time Mon Jan 2 15:04:05 MST 2006 to show the pattern with which
-	// to format/parse a given time/string.
-	p(t.Format("3:04PM"))
-    p(t.Format("Mon Jan _2 15:04:05 2006"))
-    p(t.Format("2006-01-02T15:04:05.999999-07:00"))
-	form := "3 04 PM"
-	t2, e := time.Parse(form, "8 41 PM")
-    p(t2)
+	fmt.Print((rand.Float64()*5)+5, ",")
+    fmt.Print((rand.Float64() * 5) + 5)
+    fmt.Println()
 
-	// For purely numeric representations you can also use standard string formatting
-	// with the extract components of the time value.
-	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
-        t.Year(), t.Month(), t.Day(),
-        t.Hour(), t.Minute(), t.Second())
+	// If you want a known seed, create a new rand.
+	// Source and pass it into the New constructir,
+	// NewPCG creates a new PCG source that requires a seed
+	// of two uint64 numbers.
+	s2 := rand.NewPCG(42, 1024)
+	r2 := rand.New(s2)
+	fmt.Print(r2.IntN(100), ",")
+	fmt.Print(r2.IntN(100))
+	fmt.Println()
 
-	// Parse will return an error on malformed input explaining the parsing problem.
-	ansic := "Mon Jan _2 15:04:05 2006"
-	_, e = time.Parse(ansic, "8:41PM")
-	p(e)
+	s3 := rand.NewPCG(42, 1024)
+	r3 := rand.New(s3)
+	fmt.Print(r3.IntN(100), ",")
+	fmt.Print(r3.IntN(100))
+	fmt.Println()
 }
