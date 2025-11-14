4d390fd65abbd08d414a93c47addc1ac6ebe524c
Alex McCullough <alex.mccullough@kainos.com>
2025-11-14 10:35:01 +0000

    directories

--- main.go
+++ main.go
@@ -2,56 +2,84 @@ package main
 
 import (
     "fmt"
+    "io/fs"
+    "os"
     "path/filepath"
-    "strings"
 )
 
-// The filepath prackage provides functions to parse and construct file paths
-// in a way that is portable between OSes.
+// Go has several useful functions for working with directories in the file system.
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
 
-	// Join should be used to construct paths is a portable way. It takes any
-	// number of arguments and constructs a hierarchial path from them.
-    p := filepath.Join("dir1", "dir2", "filename")
-    fmt.Println("p:", p)
+	// Creating a new sub-directory in the current working directory.
+    err := os.Mkdir("subdir", 0755)
+    check(err)
 
-	// You should always use Join instead of concatenating /s or \s manually. In
-	// addition to the portability benefits, Join will normalise paths by removing
-	// superfluous separators and directly changes.
-    fmt.Println(filepath.Join("dir1//", "filename"))
-    fmt.Println(filepath.Join("dir1/../dir1", "filename"))
+	// It's good practice to defer the removal of temporary directories.
+	// RemoveAll will not only remove a directory but its contents as well 
+	// like `rm -rf`.
+    defer os.RemoveAll("subdir")
 
-	// Dir and Base can be used split a path to the directory and the file. Alternatively
-	//, Split will return will both in the same call.
-    fmt.Println("Dir(p):", filepath.Dir(p))
-    fmt.Println("Base(p):", filepath.Base(p))
-
-	// Here we can check if a filepath is absolute.
-    fmt.Println(filepath.IsAbs("dir/file"))
-    fmt.Println(filepath.IsAbs("/dir/file"))
-
-    filename := "config.json"
-
-	// Some file names have extensions following a dot. We can split the extension out of
-	// such names with Ext.
-    ext := filepath.Ext(filename)
-    fmt.Println(ext)
-
-	// To find the file's name with the extension removed, use strings.TrimSuffix.
-    fmt.Println(strings.TrimSuffix(filename, ext))
-
-	// Rel finds a relative path between a base and a target. It returns an error if the
-	// target cannot be made relative to base.
-    rel, err := filepath.Rel("a/b", "a/b/t/file")
-    if err != nil {
-        panic(err)
+	// Helper to create an empty file.
+    createEmptyFile := func(name string) {
+        d := []byte("")
+        check(os.WriteFile(name, d, 0644))
     }
-    fmt.Println(rel)
 
-    rel, err = filepath.Rel("a/b", "a/c/t/file")
-    if err != nil {
-        panic(err)
+    createEmptyFile("subdir/file1")
+
+	// We can create a hierarchy of directories, including parentsd with MkdirAll.
+	// This is simlar to `mkdir -p`.
+    err = os.MkdirAll("subdir/parent/child", 0755)
+    check(err)
+
+    createEmptyFile("subdir/parent/file2")
+    createEmptyFile("subdir/parent/file3")
+    createEmptyFile("subdir/parent/child/file4")
+
+	// ReadDir lists directory contents, returning a slice of os.DirEntry objects.
+    c, err := os.ReadDir("subdir/parent")
+    check(err)
+
+    fmt.Println("Listing subdir/parent")
+    for _, entry := range c {
+        fmt.Println(" ", entry.Name(), entry.IsDir())
     }
-    fmt.Println(rel)
+
+	// Now we'll see the contents of subdir/parent/child when listing the current
+	// directory.
+    err = os.Chdir("subdir/parent/child")
+    check(err)
+
+    c, err = os.ReadDir(".")
+    check(err)
+
+    fmt.Println("Listing subdir/parent/child")
+    for _, entry := range c {
+        fmt.Println(" ", entry.Name(), entry.IsDir())
+    }
+
+	// cd back to where we started.
+    err = os.Chdir("../../..")
+    check(err)
+
+	// We can also visit a directory relcusively, including all its sub-directories.
+	// WalkDir attempts a callback function to handle every file or directory visited.
+    fmt.Println("Visiting subdir")
+    err = filepath.WalkDir("subdir", visit)
+}
+
+// visit is called for every file or directory found recursively by filepath.WalkDir.
+func visit(path string, d fs.DirEntry, err error) error {
+    if err != nil {
+        return err
+    }
+    fmt.Println(" ", path, d.IsDir())
+    return nil
 }
