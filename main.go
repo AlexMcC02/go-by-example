08ed336b230b40fdc238f027250a6342b2c5ebd3
Alex McCullough <alex.mccullough@kainos.com>
2025-11-14 09:55:23 +0000

    file-paths

--- main.go
+++ main.go
@@ -1,31 +1,57 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"bufio"
-	"strings"
+    "fmt"
+    "path/filepath"
+    "strings"
 )
 
-// A line filter is a common type of program that reads input on stdin, process it,
-// and then prints some derived result to stdout. grep and sed are common line filters.
+// The filepath prackage provides functions to parse and construct file paths
+// in a way that is portable between OSes.
 
-// This example has a line filter that writes a capitalised version of provided input text.
 func main() {
 
-	// Wrapping the unbuffered os.Stdin with a buffered scanner provides a convenient 
-	// Scan method that advances the scanner to the next token (the nextg line in the
-	// default scanner).
-	scanner := bufio.NewScanner(os.Stdin)
+	// Join should be used to construct paths is a portable way. It takes any
+	// number of arguments and constructs a hierarchial path from them.
+    p := filepath.Join("dir1", "dir2", "filename")
+    fmt.Println("p:", p)
 
-	for scanner.Scan() {
-		ucl := strings.ToUpper(scanner.Text())
-		fmt.Println(ucl)
-	}
+	// You should always use Join instead of concatenating /s or \s manually. In
+	// addition to the portability benefits, Join will normalise paths by removing
+	// superfluous separators and directly changes.
+    fmt.Println(filepath.Join("dir1//", "filename"))
+    fmt.Println(filepath.Join("dir1/../dir1", "filename"))
 
-	// Error check that gracefully exits the application.
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "error:", err)
-		os.Exit(1)
-	}
+	// Dir and Base can be used split a path to the directory and the file. Alternatively
+	//, Split will return will both in the same call.
+    fmt.Println("Dir(p):", filepath.Dir(p))
+    fmt.Println("Base(p):", filepath.Base(p))
+
+	// Here we can check if a filepath is absolute.
+    fmt.Println(filepath.IsAbs("dir/file"))
+    fmt.Println(filepath.IsAbs("/dir/file"))
+
+    filename := "config.json"
+
+	// Some file names have extensions following a dot. We can split the extension out of
+	// such names with Ext.
+    ext := filepath.Ext(filename)
+    fmt.Println(ext)
+
+	// To find the file's name with the extension removed, use strings.TrimSuffix.
+    fmt.Println(strings.TrimSuffix(filename, ext))
+
+	// Rel finds a relative path between a base and a target. It returns an error if the
+	// target cannot be made relative to base.
+    rel, err := filepath.Rel("a/b", "a/b/t/file")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(rel)
+
+    rel, err = filepath.Rel("a/b", "a/c/t/file")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(rel)
 }
