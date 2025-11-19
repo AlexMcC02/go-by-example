eb0c2f5bc243484b11d450010cb9736b6d0e7143
Alex McCullough <alex.mccullough@kainos.com>
2025-11-19 11:41:09 +0000

    compiler-directive

--- main.go
+++ main.go
@@ -1,50 +1,33 @@
 package main
 
-import (
-    "fmt"
-    "os"
-    "path/filepath"
-)
+import "embed"
 
-// Throughout program execution, we often want to create data that isn't needed
-// after the program exits. Temporary files are directories are useful for this
-// purpose since they do not pollute the file system over time.
+// 'go:embed' is a compiler directive allowing programs to include
+// arbitrary files and folders in the Go binary at build time.
 
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
+// Embed directives accept paths relative to the directory containing
+// the Go source file. The directive embeds the contents of the file 
+// the string variable immediately following it.
+
+//go:embed folder/single_file.txt
+var fileString string
+	
+//go:embed folder/single_file.txt
+var fileByte []byte
+
+// You can embed multiple files (or folders) with wildcards.
+
+//go:embed folder/single_file.txt
+//go:embed folder/*.hash
+var folder embed.FS
 
 func main() {
+    print(fileString)
+    print(string(fileByte))
+    
+    content1, _ := folder.ReadFile("folder/file1.hash")
+    print(string(content1))
 
-    // Temporary files can be created with os.CreateTemp as illustrated below.
-    // The first arugment is the location, so "" is the current directory.
-    f, err := os.CreateTemp("", "sample")
-    check(err)
-
-    // Temporary files are assigned a name based on the second argument provided
-    // for os.CreateTemp and the rest is chosen automatically to ensure that
-    // concurrent calls will always create different file names.
-    fmt.Println("Temp file name:", f.Name())
-
-    // Cleaning up the temporary file once we're done with it.
-    defer os.Remove(f.Name())
-
-    // Writing some data to the temporary file.
-    _, err = f.Write([]byte{1,2,3,4})
-    check(err)
-
-    // We can also create a temporary directory.
-    dname, err := os.MkdirTemp("", "sampledir")
-    check(err)
-    fmt.Println("Temp dir name:", dname)
-
-    defer os.RemoveAll(dname)
-
-    // Now we can synthesize temporary file names by prefixing them with the
-    // temporary directory.
-    fname := filepath.Join(dname, "file")
-    err = os.WriteFile(fname, []byte{1, 2}, 0666)
-    check(err)
+    content2, _ := folder.ReadFile("folder/file2.hash")
+    print(string(content2))
 }
