a603d7796625c570266b5324e5a9d0b7c3f88253
Alex McCullough <alex.mccullough@kainos.com>
2025-11-20 09:54:01 +0000

    http-server

--- main.go
+++ main.go
@@ -1,34 +1,41 @@
 package main
 
 import (
-    "bufio"
     "fmt"
     "net/http"
 )
 
-// The Go standard library comes with excellent support for HTTP clients and 
-// servers in the net/http package.
+// A fundamental concept in net/http servers is handlers. A handler is an
+// object implementing the http.Handler interface. A common way to write
+// handlers is by using the http.HandlerFunc adapter on functions with the
+// appropriate signature.
+func hello(w http.ResponseWriter, req *http.Request) {
+    
+    // Functions serving as ahandlers take a http.RequestWriter and a
+    // http.Request as arguments. The response writer is used to fill in the
+    // HTTP response.
+    fmt.Fprintf(w, "hello\n")
+}
+
+// This handler reads all the HTTP requst headers and echoes them into the
+// response body.
+func headers(w http.ResponseWriter, req *http.Request) {
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
 
 func main() {
 
-    // Here we issue an HTTP GET request to a server. http.Get is a convenient
-    // shortcut around creating an http.Client object and calling its Get method,
-    // with it invoking a http.DefaultClient object that contains sensible defaults.
-    resp, err := http.Get("https://gobyexample.com")
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
+    // We register our handlers on server routes using the http.HandleFunc
+    // convenience function. It sets up the default router in the net/http
+    // package and takes a function as an argument.
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/headers", headers)
 
-    fmt.Println("Response status:", resp.Status)
-
-    // Here we output the first five lines of the response body.
-    scanner := bufio.NewScanner(resp.Body)
-    for i := 0; scanner.Scan() && i < 5; i++ {
-        fmt.Println(scanner.Text())
-    }
-
-    if err := scanner.Err(); err != nil {
-        panic(err)
-    }
+    // Finally, we call the ListenAndServe with the report and a handler.
+    // Supplying nil as the second argument tells it to use the default router.
+    http.ListenAndServe(":8090", nil)
 }
