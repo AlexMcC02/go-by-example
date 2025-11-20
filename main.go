0856ad94b02d339dc4047ab6f5617beb9435db85
Alex McCullough <alex.mccullough@kainos.com>
2025-11-20 13:58:25 +0000

    exit

--- main.go
+++ main.go
@@ -3,36 +3,18 @@ package main
 import (
     "fmt"
     "os"
-    "os/signal"
-    "syscall"
 )
 
-// There are times when you want a Go program to intelligently handle Unix signals.
+// Use os.Exit to immediately exit with a given status.
 
 func main() {
 
-    // Go signal notification works by sending os.Signal values on a chnnel. Note that
-    // the channel we've created here is buffered.
-    sigs := make(chan os.Signal, 1)
+    // defers will NOT run when using os.Exit, so this fmt.Println will never
+    // be called.
+    defer fmt.Println("!")
 
-    // signal.Notify registers the given channel to receive notifications of the
-    // specified signals.
-    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-    // Below demonstrates how receives can be received from a separate goroutine.
-    done := make(chan bool, 1)
-
-    // This goroutine executes a blocking receive for signals. When it gets one
-    // it'll print it out then notify the program it can finish.
-    go func() {
-        sig := <-sigs
-        fmt.Println()
-        fmt.Println(sig)
-        done <- true
-    }()
-
-    // The program will wait until it gets the expected signal before exiting.
-    fmt.Println("awaiting signal")
-    <-done
-    fmt.Println("exiting")
+    // Unlike languages such as C, Go does not use an integer return value from
+    // main to indicate exit status. To use a non-zero status, you should use 
+    // os.Exit as demonstrated below.
+    os.Exit(3)
 }
